5c524fd8d3b5f57ea3f38846254e691720519a75
Stephany Ikebudu <stephikebudu@gmail.com>
2024-05-16 11:25:17 +0100

    setup theme in app

--- src/theme.js
+++ src/theme.js
@@ -2,67 +2,227 @@ import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
 // color design tokens
-// grey
-indigo: {
-    100: "#e0e0e0",
-    200: "#c2c2c2",
-    300: "#a3a3a3",
-    400: "#858585",
-    500: "#666666",
-    600: "#525252",
-    700: "#3d3d3d",
-    800: "#292929",
-    900: "#141414"
+export const tokens = (mode) => ({
+  ...(mode === "dark" ?
+    {
+      grey: {
+        100: "#e0e0e0",
+        200: "#c2c2c2",
+        300: "#a3a3a3",
+        400: "#858585",
+        500: "#666666",
+        600: "#525252",
+        700: "#3d3d3d",
+        800: "#292929",
+        900: "#141414"
+      },
+            
+      // primary dark blue 
+      primary: {
+        100: "#d0d1d5",
+        200: "#a1a4ab",
+        300: "#727681",
+        400: "#434957",
+        500: "#141b2d",
+        600: "#101624",
+        700: "#0c101b",
+        800: "#080b12",
+        900: "#040509"
+      },
+            
+      // green accent
+      greenAccent: {
+        100: "#dbf5ee",
+        200: "#b7ebde",
+        300: "#94e2cd",
+        400: "#70d8bd",
+        500: "#4cceac",
+        600: "#3da58a",
+        700: "#2e7c67",
+        800: "#1e5245",
+        900: "#0f2922"
+      },
+            
+      //red accent
+      redAccent: {
+        100: "#f8dcdb",
+        200: "#f1b9b7",
+        300: "#e99592",
+        400: "#e2726e",
+        500: "#db4f4a",
+        600: "#af3f3b",
+        700: "#832f2c",
+        800: "#58201e",
+        900: "#2c100f"
+      },
+            
+      // blue accent
+      blueAccent: {
+        100: "#e1e2db",
+        200: "#c3c6b7",
+        300: "#a4a992",
+        400: "#868d6e",
+        500: "#68704a",
+        600: "#535a3b",
+        700: "#3e432c",
+        800: "#2a2d1e",
+        900: "#15160f"
+      },
+    } : {
+      grey: {
+        100: "#141414",
+        200: "#292929",
+        300: "#3d3d3d",
+        400: "#525252",
+        500: "#666666",
+        600: "#858585",
+        700: "#a3a3a3",
+        800: "#c2c2c2",
+        900: "#e0e0e0",
+      },
+            
+      // primary dark blue 
+      primary: {
+        100: "#040509",
+        200: "#080b12",
+        300: "#0c101b",
+        400: "#f2f0f0",
+        500: "#141b2d",
+        600: "#434957",
+        700: "#727681",
+        800: "#a1a4ab",
+        900: "#d0d1d5",
+      },
+            
+      // green accent
+      greenAccent: {
+        100: "#0f2922",
+        200: "#1e5245",
+        300: "#2e7c67",
+        400: "#3da58a",
+        500: "#4cceac",
+        600: "#70d8bd",
+        700: "#94e2cd",
+        800: "#b7ebde",
+        900: "#dbf5ee",
+      },
+            
+      //red accent
+      redAccent: {
+        100: "#2c100f",
+        200: "#58201e",
+        300: "#832f2c",
+        400: "#af3f3b",
+        500: "#db4f4a",
+        600: "#e2726e",
+        700: "#e99592",
+        800: "#f1b9b7",
+        900: "#f8dcdb",
+      },
+            
+      // blue accent
+      blueAccent: {
+        100: "#15160f",
+        200: "#2a2d1e",
+        300: "#3e432c",
+        400: "#535a3b",
+        500: "#68704a",
+        600: "#868d6e",
+        700: "#a4a992",
+        800: "#c3c6b7",
+        900: "#e1e2db",
+      },
+        }),
+});
+
+//mui theme settings
+export const themeSettings = (mode) => {
+  const colors = tokens(mode);
+
+  return {
+    palette : {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500]
+            }
+          } : {
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100]
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+            typography: {
+              fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+              fontSize: 12,
+              h1: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 40,
+              },
+              h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 32,
+              },
+              h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 24,
+              },
+              h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 20,
+              },
+              h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 16,
+              },
+              h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+                fontSize: 14,
+              },
+            },
+    )
+    }
+  }
 }
 
-// primary dark blue 
-black: {
-    100: "#d0d1d5",
-    200: "#a1a4ab",
-    300: "#727681",
-    400: "#434957",
-    500: "#141b2d",
-    600: "#101624",
-    700: "#0c101b",
-    800: "#080b12",
-    900: "#040509"
-}
 
-// green accent
-green: {
-    100: "#dbf5ee",
-    200: "#b7ebde",
-    300: "#94e2cd",
-    400: "#70d8bd",
-    500: "#4cceac",
-    600: "#3da58a",
-    700: "#2e7c67",
-    800: "#1e5245",
-    900: "#0f2922"
-}
+// color mode context
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {}
+});
 
-//red accent
-red: {
-    100: "#f8dcdb",
-    200: "#f1b9b7",
-    300: "#e99592",
-    400: "#e2726e",
-    500: "#db4f4a",
-    600: "#af3f3b",
-    700: "#832f2c",
-    800: "#58201e",
-    900: "#2c100f"
-}
+export const useMode = () => {
+  const [mode, setMode] = useState("dark");
 
-// blue accent
-indigo: {
-    100: "#e1e2db",
-    200: "#c3c6b7",
-    300: "#a4a992",
-    400: "#868d6e",
-    500: "#68704a",
-    600: "#535a3b",
-    700: "#3e432c",
-    800: "#2a2d1e",
-    900: "#15160f"
+  const colorMode = useMemo(() => ({
+    toggleColorMode: () => setMode((prev) => (
+      prev === "light" ? "dark" : "light"
+    )),
+    []
+  }));
+
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
+  return [theme, colorMode];
 }
